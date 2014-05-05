562988a5ff6075342af7db54ab782873eee754a8
Thomas Nattestad <thomas.nattestad@gmail.com>
2014-05-05 13:02:09 -0400

    added file checking and reporting

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask
+import time	
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+	print "dickbutt"
 	toSend = ""
-	f = open((time.strftime("%d-%m-%Y")+'.txt'), 'r')
-	for line in f:
-		toSend += (line)
-	return toSend
+	if(os.path.isfile((time.strftime("%d-%m-%Y")+'.txt'))):
+		print "file found"
+		f = open((time.strftime("%d-%m-%Y")+'.txt'), 'r')
+		for line in f:
+			toSend += (line)
+		return toSend
+	else:
+		return "Report Not Found, check back later"
+
+
+if __name__ == "__main__":
+	# print hello();
+    app.run()
